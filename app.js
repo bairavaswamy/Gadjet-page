f97ba2d1afb76d4c9219324e2b4151f99fd5502d
bairavaswamy <bairavahoney@gmail.com>
2023-12-21 06:18:00 +0000

    gadgets-route

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+app.listen(3000)
+
+app.get('/gadgets', (request, response) => {
+  response.sendFile('./gadgets.html', {root: __dirname})
+})
+
+app.get('/', (request, response) => {
+  response.sendFile('./gadgets.html', {root: __dirname})
+})
+
+module.exports = app
